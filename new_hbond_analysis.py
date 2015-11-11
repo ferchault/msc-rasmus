aabb29e4ee5be25236a9d8a7a2f960a0a5a1b526
rasmus <rasmus.jakobsen.12@ucl.ac.uk>
2015-11-11 15:47:16 +0000

    surface pattern development

--- new_hbond_analysis.py
+++ new_hbond_analysis.py
@@ -5,10 +5,10 @@ from analysis.class_analysis import *
 #an updated hbond database with filled in hbonds (as in filled in up to delta)
 
 data_directory = "/home/rasmus/Dropbox/Education/UCL/fourth Year/Project/data/"
-bulk_oxygen_file = open(data_directory + "bulk_oxygen_5.out", 'r')
 hbond_file = open(data_directory + "rasmus-hbond-db.txt", 'r')
 
 analysis_obj_A = Analysis(data_directory +'input.psf', data_directory +'IOHMD-A-prod.dcd', data_directory +'input.ndx', 'A')
+analysis_obj_B = Analysis(data_directory +'input.psf', data_directory +'IOHMD-B-prod.dcd', data_directory +'input.ndx', 'B')
 
 #create oxygen list for water oxygen and A and G oxygens
 oxygen_list = analysis_obj_A.return_id_list_from_name("oxygen_A")
@@ -24,3 +24,8 @@ Hbond_db_structure = analysis_obj_A.hbond_analysis_build_structure(data_director
 Hbond_db_filled_structure = analysis_obj_A.hbond_get_filled_hbond(Hbond_db_structure, 60)
 analysis_obj_A.hbond_analysis_write_filled_hbond(Hbond_db_filled_structure, "new_hb_filled_db.out")
 
+analysis_obj_B.set_parameters(3.5, 160)
+
+Hbond_db_structure = analysis_obj_B.hbond_analysis_build_structure(data_directory + "rasmus-hbond-db.txt", oxygen_list)
+Hbond_db_filled_structure = analysis_obj_B.hbond_get_filled_hbond(Hbond_db_structure, 60)
+analysis_obj_B.hbond_analysis_write_filled_hbond(Hbond_db_filled_structure, "new_hb_filled_db.out", append=True)
