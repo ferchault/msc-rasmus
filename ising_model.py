3cbabefcf223e1f83552ab8ca535cf9f44af9d3c
ferchault <guido@vonrudorff.de>
2015-11-12 11:15:32 +0000

    Including symmetry operators on the lattice

--- ising_model.py
+++ ising_model.py
@@ -22,6 +22,7 @@ class IsingEstimator(object):
 	def feed_pattern(self, pattern, frequency):
 		assert (set(pattern) <= {-1, 1})
 		assert (len(pattern) == self._site_count)
+		frequency = float(frequency)
 		assert (frequency > 0)
 		self._patterns.append(pattern)
 		try:
@@ -122,6 +123,83 @@ def testIsingEstimator():
 	assert (abs(ie.gradloglikelihood(1., 1.)[1] + 3.85819419613) < 0.0000001)
 
 
+def hematite_isomorphisms(iterable):
+	iterable = list(iterable)
+	assert(len(iterable) == 12)
+	a, b, c, d = zip(*[iter(iterable)]*3)
+	isomorphisms = (a + b + c + d, b + a + d + c, c + d + a + b, d + c + b + a)
+	if type(a[0]) == str:
+		return map(''.join, isomorphisms)
+	if type(a[0]) == int:
+		return isomorphisms
+	raise NotImplementedError('Unknown type %s' % type(a[0]))
+
+def test_hematite_isomorphisms():
+	# string parameters
+	t1 = hematite_isomorphisms('abcdefghijkl')
+	assert(t1 == ['abcdefghijkl', 'defabcjklghi', 'ghijklabcdef', 'jklghidefabc'])
+
+	# integer parameters
+	t2 = hematite_isomorphisms([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1, -2])
+	assert(t2 == ((1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1, -2), (4, 5, 6, 1, 2, 3, 0, -1, -2, 7, 8, 9), (7, 8, 9, 0, -1, -2, 1, 2, 3, 4, 5, 6), (0, -1, -2, 7, 8, 9, 4, 5, 6, 1, 2, 3)))
+
+def hexagonal_move_up(iterable):
+		return [iterable[_] for _ in (5, 0, 10, 2, 3, 7, 11, 6, 4, 8, 9, 1)]
+
+def hexagonal_move_down(iterable):
+		return [iterable[_] for _ in (4, 8, 0, 1, 11, 3, 10, 2, 6, 7, 5, 9)]
+
+def hexagonal_unique_transformations(iterable):
+	iterable = tuple(iterable)
+	assert(len(iterable) == 12)
+	def _use_mapping(base, mapping):
+		return tuple([base[_-1] for _ in mapping])
+	mappings = [
+		# mirroring
+		(1, 5, 6, 4, 2, 3, 10, 8, 9, 7, 11, 12),
+		(4, 2, 3, 1, 5, 6, 7, 11, 12, 10, 8, 9),
+		(1, 2, 9, 10, 11, 6, 7, 8, 4, 3, 5, 12),
+		(7, 8, 3, 4, 5, 12, 1, 2, 9, 10, 11, 6),
+		(10, 2, 12, 4, 8, 6, 7, 5, 9, 1, 11, 3),
+		(1, 11, 3, 7, 5, 9, 4, 8, 6, 10, 2, 12),
+		# rotation
+		(12, 11, 7, 3, 2, 4, 9, 8, 10, 6, 5, 1),
+		(12, 2, 10, 9, 5, 7, 6, 8, 4, 3, 11, 1),
+		(1, 5, 9, 7, 11, 3, 10, 8, 6, 4, 2, 12),
+	]
+	matches = [iterable]
+	for mapping in mappings:
+		base = _use_mapping(iterable[:], mapping)
+		while base != iterable:
+			matches.append(base)
+			base = _use_mapping(base, mapping)
+
+	return set(matches)
+
+def hexagonal_ising_isomorphisms(iterable):
+	iterable = list(iterable)
+	assert(len(iterable) == 12)
+
+	matches = []
+	base = iterable[:]
+	for shift_up in range(6):
+		base2 = base[:]
+		for shift_down in range(6):
+			matches += hexagonal_unique_transformations(base2)
+			base2 = hexagonal_move_down(base2)
+		base = hexagonal_move_up(base)
+
+	return set(matches)
+
+def test_hexagonal_isomorphisms():
+	# test lattice vector shifts
+	assert(hexagonal_move_up(range(12)) == [5, 0, 10, 2, 3, 7, 11, 6, 4, 8, 9, 1])
+	assert(hexagonal_move_down(range(12)) == [4, 8, 0, 1, 11, 3, 10, 2, 6, 7, 5, 9])
+	assert(len(hexagonal_ising_isomorphisms(range(12))) == 144)
+
+# run tests
+test_hematite_isomorphisms()
+test_hexagonal_isomorphisms()
 testIsingEstimator()
 
 # build hematite adjacency matrix
@@ -139,8 +217,8 @@ for j in range(0, 3):
 	nb_list[:, j + 9] = d + c + b + a
 ## END CODE FROM RASMUS
 
-# take preprocessed list of patterns found and build model
-patterns = {
+# previous analysis results
+rawpatterns = {
 	'110100100100': 1425, '010010101100': 117, '001011101100': 34, '001011101101': 10, '110100110100': 2338,
 	'110110101100': 2, '111010101101': 22, '111010101100': 367, '110100110000': 4, '110100111110': 3,
 	'111100100100': 21, '001010101100': 83, '110100101100': 77, '000101101101': 10, '110100100110': 2200,
@@ -148,90 +226,113 @@ patterns = {
 	'011010101101': 16, '110110100100': 204, '110100110110': 2282, '110010101100': 106, '110100111100': 120,
 	'011011101101': 19, '011011101100': 4, '110100101110': 27}
 
-hematitematches = []
-# start estimation
+# build hematite symmetry analogues
+hematitepatterns = dict()
+hematitelookup = dict()
+for pattern in rawpatterns:
+	variants = set()
+	for elem in hematite_isomorphisms(pattern):
+		variants.update(hexagonal_ising_isomorphisms(elem))
+	for variant in variants:
+		hematitepatterns[variant] = float(rawpatterns[pattern])/len(variants)
+		hematitelookup[''.join(variant)] = pattern
+print 'The %d configurations observed map to %d configuration in the hexagonal Ising model' % (len(rawpatterns), len(hematitepatterns))
+
+# prepare estimator
 ie = IsingEstimator(nb_list)
-for pattern, count in patterns.iteritems():
-	# build equivalent patterns
-	a, b, c, d = map(''.join, zip(*[iter(pattern)]*3))
-	p1 = ''.join((a, b, c, d))
-	assert(p1 == pattern)
-	p2 = ''.join((d, c, b, a))
-	p3 = ''.join((b, a, d, c))
-	p4 = ''.join((c, d, a, b))
-	ps = set((p1, p2, p3, p4))
-	for elem in ps:
-		hematitematches.append(elem)
+for pattern, count in hematitepatterns.iteritems():
+	# convert visual to Ising representation
+	pattern = map(lambda _: 1 if _ == '1' else -1, pattern)
 
-	# convert from visual to Ising representation
-	for elem in ps:
-		elem = map(lambda _: 1 if _ == '1' else -1, elem)
-		ie.feed_pattern(elem, max(1, int(count/float(len(ps)))))
-
+	ie.feed_pattern(pattern, count)
 ie.update_cache()
-#print sco.minimize(ie.optimize_loglikelihood, (1., 2.), jac=ie.optimize_gradloglikelihood)
-#print ie.loglikelihood( 2.63644577e-01,  -1.39931567e-08), ie.gradloglikelihood( 2.63644577e-01,  -1.39931567e-08)
-alpha = 1.
-beta = 1.
 
+# estimate
+alpha, beta = 1., 1.
 for i in range(1000):
 	curval = ie.loglikelihood(alpha, beta)
 	curgrad = ie.gradloglikelihood(alpha, beta)
 	steplength = 0.000001
 	nextval = ie.loglikelihood(alpha+curgrad[0]*steplength, beta+curgrad[1]*steplength)
 	diff = curval-nextval
-	#print diff, curgrad
-	alpha+=curgrad[0]*steplength
-	beta+=curgrad[1]*steplength
+	alpha += curgrad[0]*steplength
+	beta += curgrad[1]*steplength
 	alpha = abs(alpha)
 	beta = abs(beta)
 	if diff > -0.002:
 		steplength /= 10
 
+# output
 print 'Optimal Parameters'
 print 'alpha', alpha, 'beta', beta
+print 'gradient here', ie.gradloglikelihood(alpha, beta)
 
 print
 
-print 'probabilities and frequencies for observed patterns'
-print '  pattern, freq, log(probability)'
-lgps = ie.logprobabilities(alpha, beta)
-pentries = ie._patterns
-pfreq = ie._frequencies
-for p, freq, ps in sorted(zip(pentries, pfreq, lgps), key=lambda x: x[1], reverse=True):
-	strpattern = ''.join(map(lambda _: str(_), np.maximum(p, 0)))
-	print strpattern, freq, ps
+# print 'probabilities and frequencies for observed patterns'
+# print '  pattern, freq, log(probability)'
+# lgps = ie.logprobabilities(alpha, beta)
+# pentries = ie._patterns
+# pfreq = ie._frequencies
+# for p, freq, ps in sorted(zip(pentries, pfreq, lgps), key=lambda x: x[1], reverse=True):
+# 	strpattern = ''.join(map(lambda _: str(_), np.maximum(p, 0)))
+# 	print strpattern, freq, ps
 
-
-print '50 most probable configurations (roughly top 1%)'
-z = ie._calc_z(alpha, beta)
+print '50 most probable configurations of all 2^12 mapped back to their raw hematite pattern (if it exists)'
 ie.clear()
-fullpatternlist = []
 for pattern in it.product((-1, 1), repeat=12):
-	strpattern = ''.join(map(lambda _: str(_), np.maximum(pattern, 0)))
-	a, b, c, d = map(''.join, zip(*[iter(strpattern)]*3))
-
-	# ignore equivalent (in the hematite case) patterns
-	if ''.join((d, c, b, a)) in fullpatternlist:
-		continue
-	if ''.join((b, a, d, c)) in fullpatternlist:
-		continue
-	if ''.join((c, d, a, b)) in fullpatternlist:
-		continue
-
 	ie.feed_pattern(pattern, 1)
-	fullpatternlist.append(strpattern)
 ie.update_cache()
-print 'unique pattern count', len(fullpatternlist)
 lgps = ie.logprobabilities(alpha, beta)
-assert(len(lgps) == len(fullpatternlist))
+fullpatternlist = [''.join(['1' if _ == 1 else '0' for _ in pattern]) for pattern in ie._patterns]
 
-#import matplotlib.pyplot as plt
-#plt.plot(range(len(lgps)), sorted(lgps))
-#plt.show()
-
-#print hematitematches
-ordered = sorted(zip(lgps, fullpatternlist), key=lambda x: x[0], reverse=True)[:50]
+# print hematitematches
+ordered = sorted(zip(lgps, fullpatternlist), key=lambda x: x[0], reverse=True)
+printed = list()
 print 'best/worst probability', max(lgps), min(lgps)
+print '# pattern, log(probability), raw hematite pattern, hematite count'
+pvals = []
+hemcount = []
 for ps, pattern in ordered:
-	print pattern, ps, pattern in hematitematches
+	if pattern in hematitelookup:
+		ref = hematitelookup[pattern]
+		if ref not in printed:
+			print pattern, ps, ref, rawpatterns[ref]
+			pvals.append(ps)
+			hemcount.append(rawpatterns[ref])
+			printed.append(ref)
+	else:
+		variants = set()
+		for elem in hematite_isomorphisms(pattern):
+			variants.update(hexagonal_ising_isomorphisms(elem))
+		already_printed = False
+		for variant in variants:
+			if ''.join(variant) in printed:
+				already_printed = True
+				break
+		if not already_printed:
+			print pattern, ps, 'NOHEMATITE', 'NOCOUNT'
+			pvals.append(ps)
+			hemcount.append(0)
+			printed.append(pattern)
+
+import matplotlib.pyplot as plt
+plt.plot(range(len(pvals)), pvals, '-', color='black', label='Ising Model')
+hemhas_idx = []
+hemhas_p = []
+hemhas_idx_common = []
+hemhas_p_common = []
+for idx, combo in enumerate(zip(pvals, hemcount)):
+	p, h = combo
+	if h > 0:
+		hemhas_idx.append(idx)
+		hemhas_p.append(p)
+	if h > 1000:
+		hemhas_idx_common.append(idx)
+		hemhas_p_common.append(p)
+plt.scatter(hemhas_idx, hemhas_p, color='orange', label='Hematite (all)', s=100)
+plt.scatter(hemhas_idx_common, hemhas_p_common, color='red', label='Hematite (frequent)', s=50)
+plt.xlabel('Index of configuration')
+plt.ylabel('Ln (Probability)')
+plt.legend()
+plt.show()
