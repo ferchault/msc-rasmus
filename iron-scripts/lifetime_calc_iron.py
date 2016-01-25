eda35c9548bf4049b78dcb2907e9a7cd2311fdb0
rasmus <rasmus.jakobsen.12@ucl.ac.uk>
2016-01-25 18:12:25 +0000

    End of day commit (none working state) : started the work on adding bulk water lifetime

--- iron-scripts/lifetime_calc_iron.py
+++ iron-scripts/lifetime_calc_iron.py
@@ -14,9 +14,9 @@ def calc_avg_lifetime(hbond_db, donor_list, acceptor_list):
 
     for traj in hbond_db:
         if traj == 'A':
-            nframes = 4421
+            nframes = 4570
         elif traj == 'B':
-            nframes = 4288
+            nframes = 1904
         for donor in hbond_db[traj]:
             if donor in donor_list:
                 for frame in hbond_db[traj][donor][60: nframes - 60 ]:
@@ -91,6 +91,7 @@ def load_hbond_database(hbond_file, full_oxygen_list):
 
 
 filled_hb_file = open(base_path + "hb_filled_db.out")
+bulk_water_db = open(base_path + "bulk_oxygen_7.out")
 
 analysis_obj_A = Analysis(base_path + 'IO3-index.xyz', base_path + 'IO3-A.dcd', base_path + 'input.ndx', 'A')
 
@@ -116,3 +117,64 @@ print "w i"
 lifetime_data_w_i = calc_avg_lifetime(hbond_db, water_oxygen, interface_oxygen)
 print "w w (all)"
 lifetime_data_w_w = calc_avg_lifetime(hbond_db, water_oxygen, water_oxygen)
+
+
+def calc_avg_lifetime_bulk(hbond_db_file, bulk_water_db):
+
+    for line in hbond_db_file:
+        lineparts = line.strip().split()
+
+
+    avg_lifetime = 0
+    hbond_list = dict()
+    hbond_list['A'] = []
+    hbond_list['B'] = []
+
+    for traj in hbond_db:
+        if traj == 'A':
+            nframes = 4570
+        elif traj == 'B':
+            nframes = 1904
+        for donor in hbond_db[traj]:
+            if donor in donor_list:
+                for frame in hbond_db[traj][donor][60: nframes - 60 ]:
+                        if frame.has_hbond:
+                            for hb in frame.Hbonds:
+                                if hb.acceptor in acceptor_list:
+                                    found = False
+                                    for hb_list in hbond_list[traj]:
+
+                                        if hb == hb_list.hbond:
+                                            hb_list.framelist.append(frame.frame)
+                                            found = True
+                                    if not found:
+                                        hbond_list[traj].append(HbondStrcture(hb, frame.frame))
+
+    unique_hb = []
+    for traj in hbond_list:
+        for hb in hbond_list[traj]:
+            hb.framelist = np.array(hb.framelist)
+            parts = split_array_by_delta(hb.framelist, 1)
+            unique_hb.append(parts)
+
+    lifetimes = []
+    for outer_array in unique_hb:
+        for inner_array in outer_array:
+            lifetimes.append(inner_array[-1] - inner_array[0])
+
+    lifetimes.sort()
+    lifetimes = np.array(lifetimes)
+
+    avg_lifetime = np.mean(lifetimes)*0.5
+    median_lifetime = np.median(lifetimes)*0.5
+
+    print "median", median_lifetime
+    print "average" , avg_lifetime
+    print "max" ,np.max(lifetimes)*0.5
+    print "min" ,np.min(lifetimes)*0.5
+    print "25 percentile", np.percentile(lifetimes,25)*0.5
+    print "75 percentile" ,np.percentile(lifetimes,75)*0.5
+    return lifetimes*0.5
+
+print "w w (bulk)"
+lifetime_data_w_w_bulk = calc_avg_lifetime_bulk(hbond_db, bulk_water_db, water_oxygen)
