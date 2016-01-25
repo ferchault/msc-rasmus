9fc1046f3938d9e1bcbce4b53723601e01e82f60
rasmus <rasmus.jakobsen.12@ucl.ac.uk>
2016-01-25 11:54:14 +0000

    Filled hydroge bond database for iron term, (and name change of file)

--- iron-scripts/filled-hb-db-iron.py
+++ iron-scripts/filled-hb-db-iron.py
@@ -1,8 +1,8 @@
 __author__ = 'rasmus'
 from analysis.class_analysis import *
 
-base_path = "C:\Users\Rasmus\ownCloud\data\iron-term-trajectory/"
-#base_path = "/home/rasmus/ownCloud/data/iron-term-trajectory/"
+#base_path = "C:\Users\Rasmus\ownCloud\data\iron-term-trajectory/"
+base_path = "/home/rasmus/ownCloud/data/iron-term-trajectory/"
 
 #Uses critera for the angle at 160 and distance 3.5 to create
 #an updated hbond database with filled in hbonds (as in filled in up to delta)
@@ -23,12 +23,12 @@ oxygen_list += analysis_obj_A.get_water_oxygen(water_id_list)
 
 analysis_obj_A.set_parameters(3.5, 160)
 
-Hbond_db_structure = analysis_obj_A.hbond_analysis_build_structure(base_path + "rasmus-hbond-db.txt", oxygen_list)
+Hbond_db_structure = analysis_obj_A.hbond_analysis_build_structure(base_path + "run.out", oxygen_list)
 Hbond_db_filled_structure = analysis_obj_A.hbond_get_filled_hbond(Hbond_db_structure, 60)
 analysis_obj_A.hbond_analysis_write_filled_hbond(Hbond_db_filled_structure, "new_hb_filled_db.out")
 
 analysis_obj_B.set_parameters(3.5, 160)
 
-Hbond_db_structure = analysis_obj_B.hbond_analysis_build_structure(base_path + "rasmus-hbond-db.txt", oxygen_list)
+Hbond_db_structure = analysis_obj_B.hbond_analysis_build_structure(base_path + "run.out", oxygen_list)
 Hbond_db_filled_structure = analysis_obj_B.hbond_get_filled_hbond(Hbond_db_structure, 60)
 analysis_obj_B.hbond_analysis_write_filled_hbond(Hbond_db_filled_structure, "new_hb_filled_db.out", append=True)
